03b2d46077d8553eb8394cda332be44b57253274
Aditya Rout <aditrout29@gmail.com>
2024-10-06 00:19:37 +0530

    1.  added back button 2. Added cutom css 3. intrgrated Post api for updatetable

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RestaurantList } from "../src/components/merchant/RestaurantList"; // Import the ProductList page
 import { RestuarantDetail } from '../src/pages/RestuarantDetail'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme= createTheme({
+  typography: {
+    fontSize: '2.8rem',
+    fontFamily: 'Georgia',
+  },
+})
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Routes>
         {/* <Route path="/login" element={<Auth />} />
@@ -14,6 +24,7 @@ function App() {
         <Route path="/merchants/items/:restaurantId" element={<RestuarantDetail/>}/>
       </Routes>
     </Router>
+    </ThemeProvider>
   );
 }
 
--- src/features/restaurantSlice.js
+++ src/features/restaurantSlice.js
@@ -1,11 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchRestaurantDetails } from "../utils/api";
+import {
+  fetchRestaurantDetails,
+  postUpdatedRestaurantDetails,
+} from "../utils/api";
 
 export const getRestaurantDetails = createAsyncThunk(
   "restaurant/getRestaurantDetails",
   async (restaurantId) => {
     try {
       const response = await fetchRestaurantDetails(restaurantId);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const addRestaurantDetails = createAsyncThunk(
+  "restaurant/addRestaurantDetails",
+  async (restaurantDetail) => {
+    try {
+      const response = await postUpdatedRestaurantDetails(restaurantDetail);
       return response;
     } catch (error) {
       console.log(error);
@@ -30,12 +45,24 @@ export const restaurantDetailSlice = createSlice({
       })
       .addCase(getRestaurantDetails.fulfilled, (state, action) => {
         state.loading = false;
-        console.log(action.payload,'payload====')
+        console.log(action.payload, "payload====");
         state.details = action.payload;
       })
       .addCase(getRestaurantDetails.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload || "Failed to fetch restaurants";
+      })
+      .addCase(addRestaurantDetails.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(addRestaurantDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.details = action.payload;
+      })
+      .addCase(addRestaurantDetails.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Failed to post restaurants";
       });
   },
 });
